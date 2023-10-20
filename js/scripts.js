84d8136766e86f717035c9e635497503b92aed68
ADILSON MARTINS DA SILVA <adilson.msilva4@senacsp.edu.br>
2023-10-20 20:59:20 -0300

    Commit_20-10-23

--- js/scripts.js
+++ js/scripts.js
@@ -36,6 +36,7 @@ while (i <= numero) {
   }
   i++;
 }
+
 /* Exercício 4: Escreva um programa em JavaScript que solicite um número ao usuário e exiba a tabuada desse número de 1 a 10. Utilize uma estrutura de repetição while ou for para calcular e exibir a tabuada do número informado pelo usuário. O resultado deve ser exibido no console.  */
 
 let numero = prompt("Digite um número para a tabuada: ");
